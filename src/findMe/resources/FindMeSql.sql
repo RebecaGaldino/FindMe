b655a27413dc48b968ac8f148c5782bb74769543
vinicius <vini-farias@hotmail.com>
2016-02-12 21:33:58 -0300

    *Criação dos inserts, drops e deletes das tabelas person e aluno.*

--- src/findMe/resources/FindMeSql.sql
+++ src/findMe/resources/FindMeSql.sql
@@ -1,12 +1,12 @@
-CREATE SCHEMA findme;
+create schema findme;
 
 USE findme;
 
 CREATE TABLE person(
 	id VARCHAR(11) NOT NULL,
     cpf VARCHAR(14) NOT NULL,
-    birth_dt DATE NOT NULL,
-    name VARCHAR(100) NOT NULL,
+    birth_dt datetime NOT NULL,
+    namePerson VARCHAR(100) NOT NULL,
     
     PRIMARY KEY (id)
 );
@@ -26,18 +26,50 @@ CREATE TABLE schoolsubject(
 );
 
 CREATE TABLE student (
-    course VARCHAR(30),
-    grade VARCHAR(4),
-  	id  VARCHAR(11) NOT NULL,
-   	PRIMARY KEY(id),
-    CONSTRAINT fk_student_person  FOREIGN KEY (id) REFERENCES person(id)
+     course VARCHAR(30),
+     grade VARCHAR(4),
+  	 id  VARCHAR(11) NOT NULL,
+   	 PRIMARY KEY(id),
+     CONSTRAINT fk_student_person  FOREIGN KEY (id) REFERENCES person(id)
 );
-	 
+
 CREATE TABLE monitor(
 	id VARCHAR(11) NOT NULL,
 	worktime DATETIME NOT NULL,
     roomwork VARCHAR(50) NOT NULL,
     
     PRIMARY KEY(id),
-    CONSTRAINT fk_monitor_person  FOREIGN KEY (id) REFERENCES person(id)
+    CONSTRAINT fk_monitor_student  FOREIGN KEY (id) REFERENCES student(id)
 );
+
+
+insert into person (id, cpf, birth_dt, namePerson)
+	values("20141004019", "111.222.333-44", '1999-11-07', "Maria"),
+			("20151004018", "555.666.777-88", '1999-11-07', "Joao"),
+			("20131004017", "999.111.000-11", '1998-10-06', "Pedro");
+
+insert into student (course, grade, id)
+	values("Informática", "2ano", "20141004019"),
+			("Mineração", "1ano", "20151004018"),
+			("Petróleo e gás", "3ano", "20131004017");
+
+insert into supervisor(id)
+	values("20141004019"),
+		("20151004018"),
+        ("20131004017");
+        
+delete from student 
+where id = "20141004019" and id = "20141004018" and id = "20141004017";
+
+delete from person
+where id = "20141004019" and id = "20141004018" and id = "20141004017";
+
+drop table monitor;
+
+drop table student;
+
+drop table supervisor;
+
+drop table person;
+
+drop schema findme;
