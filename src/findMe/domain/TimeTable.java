d0f17791529c6b07160ec920cf2387b587006590
RebeacGaldino <rebecaolg@gmail.com>
2016-02-26 16:43:38 -0300

    Create timetable

--- src/findMe/domain/TimeTable.java
+++ src/findMe/domain/TimeTable.java
@@ -1,6 +1,44 @@
 package findMe.domain;
 
+import java.sql.Date;
+
 public class TimeTable {
+	private int id;
+	private String dayName;
+	private Date begin;
+	private Date end;
+	// private Monitor monitor;  --> Colocamos?
+	
+	
+	
+	
+	/*---------------------------------------------------------------------------------------------------------*/
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDayName() {
+		return dayName;
+	}
+	public void setDayName(String dayName) {
+		this.dayName = dayName;
+	}
+	public Date getBegin() {
+		return begin;
+	}
+	public void setBegin(Date begin) {
+		this.begin = begin;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	
 	
 	
 }
