7184fa97caa9468b24375269d88995f99ac8c52f
ViniFarias <vini-farias@hotmail.com>
2016-03-01 14:42:38 -0300

    Update Monitor.java

--- src/findMe/domain/Monitor.java
+++ src/findMe/domain/Monitor.java
@@ -6,13 +6,16 @@ public class Monitor extends Student{
 	
 	private SchoolSubject subject;
 	
-	public Monitor(String id, String cpf, Date birth_dt, String name,
-			String course, String year, SchoolSubject subject) {
+	public Monitor(String id, String cpf, Date birth_dt, String name, String course, String year, SchoolSubject subject) {
 		super(id, cpf, birth_dt, name, course, year);
 		setSubject(subject);
 	}
 	
-	
+	//Cria um monitor com o id do supervisor
+	/*public Monitor(String id, String cpf, Date birth_dt, String name, String course, String year, SchoolSubject subject, supervisor.getId()) {
+		super(id, cpf, birth_dt, name, course, year);
+		setSubject(subject);
+	}*/
 	/*---------------------------------------------------------------------------------------------------------*/
 	
 	public SchoolSubject getSubject() {
