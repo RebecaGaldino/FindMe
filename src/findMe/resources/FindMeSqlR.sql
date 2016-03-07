40641f9678b39b9b11353aeb46b5640bfd7e1b32
Rebeca Galdino <rebecaolg@gmail.com>
2016-03-07 18:19:55 -0300

    Updates - Selects

--- src/findMe/resources/FindMeSqlR.sql
+++ src/findMe/resources/FindMeSqlR.sql
@@ -29,6 +29,7 @@ WHERE A1.id = A2.id
 
 GROUP BY A2.namePerson;
 
+
 /* Select the monitor's name and his subject  */
 SELECT A2.namePerson, A3.namesubject
 
@@ -37,3 +38,16 @@ FROM supervisor_schoolsubject A1, person A2, schoolsubject A3
 WHERE A2.id = A1.id_supervisor and A3.id = A1.id_schoolsubject
 
 GROUP BY A2.namePerson;
+
+/* Select the monitor's name and the timetables related to him */
+SELECT A1.namePerson, A2.dayname, A2.begin_time, A2.end_time
+
+FROM person A1, timetable A2
+
+WHERE A1.id = A2.id_monitor
+
+GROUP BY A1.namePerson 
+
+/* ORDER BY A2.dayname(A2.dayname, 'Segunda-feira', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado', 'Domingo'); - Tenta se isso funciona pra ordenar pelo dia */
+
+
