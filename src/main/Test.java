d0dd62885da994fbf7f2bb6ffe11c618729b6f41
Rebeca Galdino <rebecaolg@gmail.com>
2016-03-07 18:24:01 -0300

    Updates - Selects

--- src/main/Test.java
+++ src/main/Test.java
@@ -5,24 +5,24 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-import findMe.dataBase.PersonManipulateDB;
-import findMe.dataBase.SupervisorManipulateDB;
+import findMe.dataBase.PersonDAO;
+import findMe.dataBase.SupervisorDAO;
 import findMe.domain.Person;
 import findMe.domain.Supervisor;
 
 public class Test {
 	public static void main(String[] args) throws ParseException {
-		PersonManipulateDB mdb = new PersonManipulateDB();
+		PersonDAO mdb = new PersonDAO();
 		String inputStr = "11-11-2012";
 		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 		Date inputDate = dateFormat.parse("11-11-2012");
-		SupervisorManipulateDB s = new SupervisorManipulateDB();
+		SupervisorDAO s = new SupervisorDAO();
 		
 		Person person = new Person("202020", "083.738.794-98", inputDate, "thayanne");
 		mdb.insertPerson(person);
 		
-		s.insertSupervisor(id, name);
-		Supervisor supervisor = new Supervisor();
+		//s.insertSupervisor(id, name);
+		//Supervisor supervisor = new Supervisor();
 		
 	}
 	
