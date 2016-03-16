58c9ab1da270b0d785822d0d2fe1fe4c3ee3742f
vinicius <vini-farias@hotmail.com>
2016-03-15 22:06:55 -0300

    UI

--- src/findMeUI/RegisterMonitorScreenFXMLController.java
+++ src/findMeUI/RegisterMonitorScreenFXMLController.java
@@ -1,10 +1,15 @@
 package findMeUI;
 
 import java.util.ResourceBundle;
+
+import javax.swing.event.ChangeListener;
+
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextField;
 import java.io.IOException;
 import java.net.URL;
@@ -18,10 +23,15 @@ import javafx.stage.Stage;
 public class RegisterMonitorScreenFXMLController implements Initializable{
 
 	//@FXML private Button btIniciar;
+	@FXML private ScrollBar scrollBar;
 	
 	@FXML
 	/*Método que exibe a tela de seleção das opções (menu)*/
 	private void iniciar(ActionEvent event) throws IOException{
+		
+	}
+	
+	private void onScrollBar(ActionEvent event) throws IOException{
 		
 	}
 	
