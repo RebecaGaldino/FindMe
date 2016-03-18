cfddc84ecd23a30558038ad92a03434a772a43c5
Thayanne Luiza <thayannevls@gmail.com>
2016-03-18 00:00:41 -0300

    RegisterUI

---
+++ src/findMeUI/RegisterSupervisorFXMLController.java
@@ -0,0 +1,14 @@
+package findMeUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RegisterSupervisorFXMLController extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
