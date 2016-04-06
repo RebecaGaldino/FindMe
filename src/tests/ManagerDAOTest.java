30e8fffe8e4de4fe4dfe45b42eb3033b43dc89ea
Rebeca Galdino <rebecaolg@gmail.com>
2016-04-06 18:33:54 -0300

    Tests

---
+++ src/tests/ManagerDAOTest.java
@@ -0,0 +1,109 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author RebecaGaldino
+ *
+ */
+public class ManagerDAOTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testManagerDAO() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetManagersInformations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserChecksManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
