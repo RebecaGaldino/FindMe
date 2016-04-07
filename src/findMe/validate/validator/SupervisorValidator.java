cd93b4059d7ace764ba4fef7fb0f90dd6061dd08
Thayanne Luiza <thayannevls@gmail.com>
2016-04-07 20:26:36 -0300

    SupervisorValidator

--- src/findMe/validate/validator/SupervisorValidator.java
+++ src/findMe/validate/validator/SupervisorValidator.java
@@ -2,6 +2,11 @@ package findMe.validate.validator;
 
 import findMe.domain.Supervisor;
 
+/**
+ * Validate Supervisor as Person
+ * @author thayanneLuiza
+ *
+ */
 public class SupervisorValidator {
 	public static boolean validate(Supervisor supervisor){
 		if(!PersonValidate.validate(supervisor))
