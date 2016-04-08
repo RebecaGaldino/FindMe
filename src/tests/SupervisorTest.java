419d16e8949ae60789f919a2a13e8bce8c6ce7c8
vinicius <vini-farias@hotmail.com>
2016-04-07 23:57:34 -0300

    - Ajustes

--- src/tests/SupervisorTest.java
+++ src/tests/SupervisorTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import findMe.actions.ManagerActions;
+import findMe.actions.SupervisorActions;
 import findMe.dataBase.SupervisorDAO;
 import findMe.domain.Supervisor;
 import findMe.extraMethods.Methods;
@@ -19,6 +20,7 @@ public class SupervisorTest {
 	Supervisor s1;
 	Supervisor s2;
 	ManagerActions mact;
+	SupervisorActions sact;
 	SupervisorDAO sdao;
 		
 	
@@ -28,6 +30,7 @@ public class SupervisorTest {
 				"Aa#Ç~12", "Masculino", "4.123.133", "vinifarias@gmail.com");
 		s2 = new Supervisor("20131002016", "18943687090", "02/06/1997", "Michelle Machado Tavares da Rocha", "Ba#k~23", "Feminino", "6.353.098", "michellemtr@gmail.com");
 		mact = new ManagerActions();
+		sact = new SupervisorActions();
 		sdao = new SupervisorDAO();
 	
 	}
@@ -49,18 +52,24 @@ public class SupervisorTest {
 		}
 	
 	}
-
-	/*@Test
+	
+	
+	@Test
+	public void testUpdateSupervisor() {
+		Supervisor saux = sdao.getSupervisorById(s1.getId());
+		saux.setName("Marcus Vinicius");
+		
+		sact.editSupervisor(saux);
+		assertEquals(sdao.getIdPerson(s1.getCpf()), s1.getId());
+	}
+	
+	
+	@Test
 	public void testDeleteSupervisor() {
 		mact.deleteSupervisor(s1);
 		assertFalse(sdao.getIdPerson(s1.getCpf()).equals(s1.getId()));
-	}*/
-
-	@Test
-	public void testUpdateSupervisor() {
-		
-		assertEquals(sdao.getIdPerson(s1.getCpf()), s1.getId());
 	}
+
 
 	@Test
 	public void testUserChecksSupervisor() {
