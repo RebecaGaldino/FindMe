6c5713a067f36198ccc81557d9b468c93977650b
vinicius <vini-farias@hotmail.com>
2016-04-08 00:37:44 -0300

    - Ajustes

---
+++ src/tests/SchoolSubjectTest.java
@@ -0,0 +1,57 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import findMe.actions.ManagerActions;
+import findMe.dataBase.SchoolSubjectDAO;
+import findMe.domain.SchoolSubject;
+import findMe.domain.Supervisor;
+import findMe.extraMethods.Methods;
+import findMe.validate.validator.SupervisorValidator;
+
+public class SchoolSubjectTest {
+	
+	SchoolSubject sb1;
+	SchoolSubject sb2;
+	ManagerActions mact;
+	SchoolSubjectDAO ssdao;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		sb1 = new SchoolSubject("Sociolgia", "123");
+		sb2 = new SchoolSubject("Quimica", "876");
+		mact = new ManagerActions();
+		ssdao = new SchoolSubjectDAO();
+	}
+
+	@Test
+	public void testInsertSchoolSubject() {
+		mact.registerSubject(sb1);
+		assertEquals(ssdao.getSchoolSubjectById(sb1.getId()).getId(), sb1.getId());
+	}
+	
+	
+	@Test
+	public void testUpdateSchoolSubject() {
+		
+	
+		SchoolSubject sbaux = ssdao.getSchoolSubjectById(sb1.getId());
+		sbaux.setName("Portugues");
+		ssdao.updateSchoolSubject(sbaux);
+		
+		assertEquals(ssdao.getSchoolSubjectById(sbaux.getId()).getId(), sbaux.getId());
+	}
+	
+	
+	@Test
+	public void testDeleteSchoolSubject() {
+		ssdao.deleteSchoolSubject(sb1.getId());
+		assertEquals(ssdao.getSchoolSubjectById(sb1.getId()).getId(), sb1.getId());
+		
+	}
+
+}
